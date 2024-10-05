f69fdc15b1da27acd5cd1c3ab3fb1cd5e8db7058
codernosunday <tin369cv@gmail.com>
2024-10-05 16:05:55 +0700

    update 5/10/2024

---
+++ client/site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Askem',
+  description: 'CNTT2',
+  siteUrl: 'https://clone-of-stackoverflow.vercel.app',
+  googleAnalytic: 'UA-149699646-1',
+  author: {
+    email: '21004102@st.vlute.edu.vn',
+    name: 'VTT'
+  },
+  lang: 'en'
+}
---
+++ client/stories/6-Modal.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import AuthenticationModal from '../components/auth-forms'
+
+export default {
+  title: 'Forms'
+}
+
+export const AuthModal = () => <AuthenticationModal />
